c8dae2529cdee172e3800609b43369b0cabc006d
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-05-01 09:45:46 +0200

    cosmetic change

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -15,6 +15,8 @@ const HomePage = () => {
             />
             <Typography variant="body1" gutterBottom sx={{ marginTop: 4 }}>
                 My name is Sławek, and I've created this website to compile and maintain an archive of "Friday Tips for Seniors" (ftfs) that I share on <Link href="https://www.linkedin.com/in/slawekradzyminski/" target="_blank" rel="noopener noreferrer">LinkedIn</Link>. Additionally, I aim to organize my book recommendations in a clear and structured manner. Please feel free to explore and enjoy the site!
+                <br /><br />
+                Tips can also be accessed directly in Markdown format from the publicly available <Link href="https://github.com/slawekradzyminski/reactapp" target="_blank" rel="noopener noreferrer">React codebase</Link>. You can view a sample tip on <Link href="https://github.com/slawekradzyminski/reactapp/blob/master/src/tips/Visibility.md" target="_blank" rel="noopener noreferrer">Visibility</Link>.
             </Typography>
         </Paper>
     );
