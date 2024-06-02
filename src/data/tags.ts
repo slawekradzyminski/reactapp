3649aaad2d98567431bbb5c4a868a394b8372996
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-06-02 13:26:26 +0200

    added the latest book recommendations

--- src/data/tags.ts
+++ src/data/tags.ts
@@ -7,7 +7,12 @@ enum Tags {
     Feynman = "Feynman",
     LeonardoDaVinci = "Leonardo da Vinci",
     Boyd = "Boyd",
-    PublicRelations = "Public Relations"
+    PublicRelations = "Public Relations",
+    Leadership = "Leadership",
+    Product = "Product",
+    Programming = "Programming",
+    Thinking = "Thinking",
+    Human = "Human",
 }
 
 export default Tags;
