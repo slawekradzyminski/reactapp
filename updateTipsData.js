1f816cf00fb54fece9eae973fb9be8d08115d6d9
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-09-29 14:25:24 +0200

    attempt to migrate to vite

--- updateTipsData.js
+++ updateTipsData.js
@@ -1,6 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const marked = require('marked');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { marked } from 'marked';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const tipsDirectory = path.join(__dirname, 'src/tips');
 const tipsDataFile = path.join(__dirname, 'src/data/tipsData.json');
@@ -19,26 +23,30 @@ marked.setOptions({
   renderer: renderer
 });
 
-fs.readdir(tipsDirectory, (err, files) => {
-  if (err) {
-    console.error('Could not list the directory.', err);
-    process.exit(1);
-  }
+const updateTipsData = () => {
+  fs.readdir(tipsDirectory, (err, files) => {
+    if (err) {
+      console.error('Could not list the directory.', err);
+      process.exit(1);
+    }
 
-  const tipsData = files
-    .filter(file => file.endsWith('.md'))
-    .map((file, index) => {
-      const filePath = path.join(tipsDirectory, file);
-      const content = fs.readFileSync(filePath, 'utf8');
-      // Convert Markdown to HTML with the custom renderer
-      const htmlContent = marked.parse(content);
-      // Extract the file name without the .md extension for the title
-      const title = file.replace('.md', '');
-      return { id: index + 1, title, content: htmlContent };
-    });
+    const tipsData = files
+      .filter(file => file.endsWith('.md'))
+      .map((file, index) => {
+        const filePath = path.join(tipsDirectory, file);
+        const content = fs.readFileSync(filePath, 'utf8');
+        // Convert Markdown to HTML with the custom renderer
+        const htmlContent = marked.parse(content);
+        // Extract the file name without the .md extension for the title
+        const title = file.replace('.md', '');
+        return { id: index + 1, title, content: htmlContent };
+      });
 
-  const output = JSON.stringify(tipsData, null, 2);
+    const output = JSON.stringify(tipsData, null, 2);
 
-  fs.writeFileSync(tipsDataFile, output);
-  console.log('tipsData.json has been updated with content from /tips folder.');
-});
+    fs.writeFileSync(tipsDataFile, output);
+    console.log('tipsData.json has been updated with content from /tips folder.');
+  });
+};
+
+updateTipsData();
