4b3846224a886e3a00b75c5aa7a984858697564e
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-09-30 08:29:12 +0200

    fix bug caused by marked update

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from "vite";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-  ],
+  plugins: [react(), viteTsconfigPaths()],
+  test: {
+    coverage: {
+      include: ["src/**"],
+    },
+  },
 });
