23244518343290505d21ebd3f03cae5cfc8991dc
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-12-29 15:09:32 +0100

    reorder

--- src/components/header/pages.ts
+++ src/components/header/pages.ts
@@ -1,7 +1,7 @@
 export const pages = [
+    { name: 'Blog', path: '/blog' },
     { name: 'Tips for Seniors', path: '/tips' },
     { name: 'Book Recommendations', path: '/books' },
-    { name: 'Blog', path: '/blog' },
     { name: 'Public Speaking', path: '/speaking' }
 ];
 
